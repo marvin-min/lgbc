e1e7a51cafa9f254fa875e1d03c1159089483ddc
marvin-min <marvin.min@activenetwork.com>
2016-10-27 23:01:10 +0800

    write cookies to client

---
+++ cookies_session/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"fmt"
+)
+
+func main() {
+	http.HandleFunc("/", sayHello)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServer: ", err)
+	}
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request)  {
+	cookie := http.Cookie{Name:"hello",Value:"world"}
+	http.SetCookie(w,&cookie)
+	fmt.Fprint(w,"Hello World")
+}
